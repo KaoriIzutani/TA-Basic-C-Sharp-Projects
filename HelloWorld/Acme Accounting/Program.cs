9ceb579ce4bf34fe239a8de9d15ad34ba05eab13
Kaori Izutani <kaoriizutani@yahoo.com>
2018-11-12 16:29:06 -0800

    Acme Accounting Exercise

---
+++ HelloWorld/Acme Accounting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Acme_Accounting
+{
+    class Program
+    {
+        static void Main()
+
+       {
+            string msg = @"Welcome to Acme Accounting Systems. 
+ Remember, we're ""accounting"" on you!";
+
+            Console.WriteLine(msg);
+            
+          
+            Console.ReadLine();
+        }
+    }
+}
