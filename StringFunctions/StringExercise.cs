b02eabad430de870510bba3f92f186d1b0496968
Kaori Izutani <kaoriizutani@yahoo.com>
2018-11-30 15:53:25 -0800

    Strings Exercise

---
+++ StringFunctions/StringExercise.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace StringFunctions
+{
+    class StringExercise
+    {
+        //1. Concatenating 3 strings:
+
+        static void Main(string[] args)
+        {
+            string quote1 = "Kaori says,\"I";
+            string quote2 = " want to be the best front end designer ";
+            string quote3 = "in Portland, OR\".";
+
+            Console.WriteLine(quote1 + quote2 + quote3);
+            Console.ReadLine();
+
+
+            //2. Convert string to uppercase:
+            string school = "The Tech Academy";
+            school = school.ToUpper();
+            Console.WriteLine(school);
+            Console.ReadLine();
+
+            //3. Create a Stringbuilder. Build a paragraph of text, one sentence at a time:
+            StringBuilder sb = new StringBuilder(50);
+            sb.Insert(0, "I like the Tech Academy. ");
+            sb.Append("Currently, there are campuses in Portland, Seattle and Denver. ");
+            sb.Append("They offer courses, such as \nSoftware Developer, Python, Web Developer and more. ");
+            sb.Append("Go to learncodinganywhere.com for more information.");
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+             }
+        }
+    }
+
--- StringFunctions/StringFunctions/Program.cs
+++ StringFunctions/StringFunctions/Program.cs
@@ -8,13 +8,32 @@ namespace StringFunctions
 {
     class Program
     {
-        static void Main(string[] args)
+        static void NotMain(string[] args)
         {
-            string name = "Kaori";
-            string quote = "The man said, \"Hello\", Kaori. \n This goes on a new line. \n \t Hello on a tab." ; 
-            /*Escape characters are indicated by the back slash. "\n" creates a new line. \n \t creates a tab.*/
+            string name = "Camus";
+            name = "Joe";
 
-            Console.WriteLine(quote);
+
+            //string quote = "The man said, \\Hello, Kaori\". \n This goes on a new line. \n \t Hello on a tab." ;
+            //string fileName = @"C:\Users\Kaori";
+            ///*Escape characters are indicated by the back slash. "\n" creates a new line. "\n \t" creates a tab.
+            // "\\ results in one backslash showing in your string.*/
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length; /*Length is a property of this string, 
+            //which we assigned the var "name"*/
+
+            name = name.ToUpper(); /*This converted what was assigned to the var "name" into 
+            upper (ToUpper property) or lower case (ToLower).*/
+
+            StringBuilder sb = new StringBuilder(); /*StringBuilder is a dynamic object. More on 
+            what a dynamic object is later on*/
+            sb.Append("My name is Kaori.");
+
+            Console.WriteLine(name);
+            Console.WriteLine(sb);
             Console.ReadLine();
         }
     }
