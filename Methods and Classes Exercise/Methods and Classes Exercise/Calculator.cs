5031f2a33dced97a6747394efa82e93a1de30339
KowKow <kaoriizutani@yahoo.com>
2019-05-03 18:42:12 -0700

    Methods and Classes Exercise

---
+++ Methods and Classes Exercise/Methods and Classes Exercise/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Classes_Exercise
+{
+/* 1. Create a class. In that class, create three methods, each of which will take one integer parameter in 
+ * and return an integer. The methods should do some math operation on the received parameter. Put this class 
+ * in a separate .cs file in the application.*/
+
+    public class Calculator
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public static int Add(int x) /*Method #1*/
+        {
+            int y = 12;
+            y = y + x;
+            return y;
+        }
+        
+        public static int Divide (int x) /*Method #2*/
+        {
+            int y = 12;
+            y = x / y;
+            return y;
+        }
+
+        public static int Multiply (int x) /*Method #3*/
+        {
+            int y = 12;
+            y = y * x;
+            return y;
+        }
+            
+        
+
+    }
+}
---
+++ Methods and Classes Exercise/Methods and Classes Exercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Classes_Exercise
+{
+    class Program
+    {
+    /*2. In the Main() program, ask the user what number they want to do the math operations on.*/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to do math on?");
+            int x = Convert.ToInt32 (Console.ReadLine());
+            int finalAnswer;
+            Console.WriteLine("You entered " + x);
+
+
+            /*3. Call each method in turn, passing the user input to the method. Display the returned integer 
+             * to the screen.*/
+
+            finalAnswer = Calculator.Add(x);
+            Console.WriteLine(x + " plus 12 equals " + finalAnswer);
+           
+
+            finalAnswer = Calculator.Divide(x);
+            Console.WriteLine(x + " divided by 12 equals " + finalAnswer);
+            
+
+            finalAnswer = Calculator.Multiply(x);
+            Console.WriteLine(x + " times 12 equals " + finalAnswer);
+            Console.ReadLine();
+        }
+    }
+}
