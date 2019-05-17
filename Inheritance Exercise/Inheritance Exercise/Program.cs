d112019ef435194839a4b3d44aaa603d8ad0f176
KowKow <kaoriizutani@yahoo.com>
2019-05-17 15:14:03 -0700

    Inheritance Exercise

---
+++ Inheritance Exercise/Inheritance Exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Exercise
+{
+   /* 4. Inside of the Main method, instantiate and initialize an Employee object with a first name of "Sample" 
+    * and a last name of "Student". & 
+    * 5. Call the superclass method SayName() on the Employee object.*/
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
