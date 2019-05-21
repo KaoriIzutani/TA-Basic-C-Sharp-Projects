2030ed70f887967e7fcd33c01af09be547dbc493
KowKow <kaoriizutani@yahoo.com>
2019-05-21 16:32:01 -0700

    Interface Exercise

---
+++ Interface Exercise/Interface Exercise/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Exercise
+{
+    /* 2. Have your Employee class from the previous drill inherit that interface and implement the Quit() method in 
+     * any way you choose.*/
+
+    public class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("Today, I am quitting my job.");
+        }
+    }
+}
+
+    
+
+
+
+
---
+++ Interface Exercise/Interface Exercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Exercise
+{
+    class Program
+    {
+       /*3. Use polymorphism to create an object of type IQuittable and call the Quit() method on it.
+        * Hint: an object can be of an interface type if it implements that specific interface.*/
+
+        static void Main(string[] args)
+        {
+            IQuittable quittable = new Employee();
+            Quit(quittable);
+            Console.ReadLine();
+
+        }
+
+        static void Quit(IQuittable quit)
+        {
+            quit.Quit();
+        }
+    }
+}
