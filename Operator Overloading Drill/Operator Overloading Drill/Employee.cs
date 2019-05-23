655e2b21ba56039ad2130e8d6484076c2e60b9e3
KowKow <kaoriizutani@yahoo.com>
2019-05-23 15:58:54 -0700

    Operator Overloading Drill

---
+++ Operator Overloading Drill/Operator Overloading Drill/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading_Drill
+{
+/* Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.*/
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+        public static bool operator ==(Employee e1, Employee e2)
+        {
+            if (e1.Id == e2.Id)
+            {
+                return true;
+             }
+
+            else
+            {
+                return false;
+
+             }
+
+        }
+
+        public static bool operator!= (Employee e1, Employee e2)
+        {
+            if (e1.Id != e2.Id)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+
+            }
+        }
+        }
+    }
+
---
+++ Operator Overloading Drill/Operator Overloading Drill/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee() { FirstName = "Kaori", LastName = "Izutani", Id = 1 };
+            Employee e2 = new Employee() { FirstName = "Kaori", LastName = "Izutani", Id = 3 };
+            Employee e3 = new Employee() { FirstName = "Kaori", LastName = "Izutani", Id = 3 };
+
+            Console.WriteLine(e1 == e3);
+            Console.WriteLine(e2 == e3);
+            Console.WriteLine(e1 != e2);
+            Console.ReadLine();
+        }
+    }
+}
