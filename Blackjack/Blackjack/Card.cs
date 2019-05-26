f7ad1bdbb066eef4c12c9c8833d2e06510a64315
KowKow <kaoriizutani@yahoo.com>
2019-05-26 16:56:35 -0700

    Added enum method

--- Blackjack/Blackjack/Card.cs
+++ Blackjack/Blackjack/Card.cs
@@ -6,21 +6,43 @@ using System.Threading.Tasks;
 
 namespace Blackjack
 {
-   public class Card /*This class was created by right clicking the project name in Solution Explorer-->Add-->Class.
-        Follow the prompts. We named this class, Card, for this project. General good practice is to keep to one class per
-        project.*/
+   public class Card /*This class was created by right clicking the project name in Solution Explorer-->Add-->
+        Class. Follow the prompts. We named this class, Card, for this project. General good practice is to keep
+        to one class per project.*/
     {
         
-            public Card()
-            {
-                Suit = "Spades";
-                Face = "Two";
-            }
+            
         
-        public string Suit { get; set; } /*Here, you created a property of the Card class. Analogy of a class (design
-        for an object) vs object would be, a class is a recipe, while object is the food actually created, cooked and 
-        eaten. */
+        public Suit2 /*After creating the Suit2 enum, data type is no longer the generic "string".
+        Data type was updated to "Suit2"*/ Suit { get; set; } /*Here, you created a property of the Card class. 
+        Analogy of a class (design for an object) vs object would be, a class is a recipe, while object is the 
+        food actually created, cooked and eaten. */
         public string Face { get; set; }
+    }
+    public enum Suit2
+    {
+        Clubs = 4, /*Like an array, this "Clubs" has an implicit value of 0. Then "Diamonds" is 1,
+        "Hearts" is 2,"Spades" is 3. You can change the value of the enums if you want.*/
+        Diamonds = 10,
+        Hearts,
+        Spades
+    }
+
+    public enum Face
+    {
+        Two,
+        Three,
+        Four, 
+        Five, 
+        Six,
+        Seven,
+        Eight, 
+        Nine,
+        Ten, 
+        Jack,
+        Queen,
+        King,
+        Ace
     }
     }
     
--- Blackjack/Blackjack/Deck.cs
+++ Blackjack/Blackjack/Deck.cs
@@ -14,27 +14,29 @@ namespace Blackjack
             Cards = new List<Card>(); /*Cards refers to the property of the class Deck. You don't need to name 
             a var or give it a data type, because Cards is a property of the object or class. So far, this is an 
             empty list or it's not substantiated yet.*/
-            
-            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
-            List<string> Faces = new List<string>()
 
-            { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
-          
-        foreach (string face in Faces) /*Here, we're creating a nested foreach loop to loop through 4 times. There 
-                are 13 faces and 4 suits, so 13 x 4 = 52 cards to create.*/
+            for (int i = 0; i < 13; i++) /*This loops through all the faces we created in the Card class.*/
             {
-                foreach (string suit in Suits)
-                {
-                    Card card = new Card(); /*Creates a new, blank card*/
-                    card.Suit = suit; /*Assigns the card one of the four suits*/
-                    card.Face = face; /*Assigns the card one of 13 faces*/
-                    Cards.Add(card); /*Adds this card to the blank list called "Cards", that was created on top*/
-                }
+                for (int j)
             }
+            
+           
+        //foreach (string face in Faces) /*Here, we're creating a nested foreach loop to loop through 4 times. There 
+        //        are 13 faces and 4 suits, so 13 x 4 = 52 cards to create.*/
+        //    {
+        //        //foreach (string suit in Suits)
+        //        //{
+        //        //    Card card = new Card(); /*Creates a new, blank card*/
+        //        //    card.Suit = suit; /*Assigns the card one of the four suits*/
+        //        //    card.Face = face; /*Assigns the card one of 13 faces*/
+        //        //    Cards.Add(card); /*Adds this card to the blank list called "Cards", that was created on top*/
+                
+        //        //}
+        //    }
         }
-        public List<Card> Cards { get; set; }
+        public List<Card> Cards { get; set; } /*Here is where you set up the Cards property*/
 
-        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 2)
+        public void Shuffle(Deck deck, out int timesShuffled, int times = 2)
         {
             timesShuffled = 0;
             for (int i = 0; i < times; i++)
