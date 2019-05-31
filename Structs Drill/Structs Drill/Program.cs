d7d9aac1d6ed35dc5bfd831c779fa66f7acd6759
KowKow <kaoriizutani@yahoo.com>
2019-05-31 15:40:06 -0700

    Structs Drill

---
+++ Structs Drill/Structs Drill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs_Drill
+{
+    class Program
+    {
+        /*2. In the Main() method, create an object of data type Number and assign an amount to it.*/
+
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 41.41m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
