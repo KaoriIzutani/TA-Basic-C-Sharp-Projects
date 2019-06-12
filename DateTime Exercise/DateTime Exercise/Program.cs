c9bbe9baebbe239bf10341a1aa80c41c5006896e
KowKow <kaoriizutani@yahoo.com>
2019-06-12 15:22:52 -0700

    Date Time Exercise

---
+++ DateTime Exercise/DateTime Exercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTime_Exercise
+{
+    class Program
+    {
+
+        /*1. Prints the current date and time to the console.
+
+          2. Asks the user for a number.
+
+          3. Prints to the console the exact time it will be in X hours, X being the number the user entered
+          in step 2.*/
+
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Right now, it is currently " + DateTime.Now);
+
+            Console.WriteLine("Enter a number: ");
+            double enteredNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine( "You've added " + enteredNumber + " hours. Now it is " 
+                + DateTime.Now.AddHours(enteredNumber));
+           
+            Console.ReadLine();
+        }
+    }
+}
